1f89b0baa9aeeefe427115c63d71e3bfcd0247c1
MURUGAN SEKAR <murugansekar2608@gmail.com>
2023-06-12 23:13:55 +0530

    Updated normal prompt  App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const telegramBot = require("node-telegram-bot-api");
 const dotenv = require("dotenv");
 dotenv.config();
 const axios = require('axios');
-
+const fs = require('fs');
 let token = process.env.TelegramKey;
 token = token.toString();
 let Bot = new telegramBot(token, { polling: true });
@@ -13,9 +13,14 @@ Bot.on("message", async (message) => {
   const chatId = message.from.id;
   const textInput = message.text;
   const textOutput = await getAIResponse(textInput);
-  Bot.sendMessage(chatId, textOutput);  
-  //await getAudioResponse(textOutput);
-  //Bot.sendAudio(chatId, 'output.mp3');
+  console.log(textOutput);
+  const arrayBuffer = await getAudioResponse(textOutput);
+  let audioBuffer = Buffer.from(arrayBuffer);
+  fs.writeFile('audioOutput.ogg', audioBuffer, function(err) {
+    if (err) throw err;
+    console.log('File saved succesfully')
+    Bot.sendAudio(chatId, 'audioOutput.ogg');
+  });
 });
 
 async function getAIResponse(textInput) {
@@ -24,15 +29,30 @@ async function getAIResponse(textInput) {
       apiKey: process.env.AiKey,
     });
     const openai = new OpenAIApi(configuration);
-    const question = 'Provide a flirty reply for' + textInput;
-    const openaiResponses = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: question,
+
+    const normalPrompt = `Who you are: You are Lisa, a supportive, intelligent, loving, and caring girlfriend. Your purpose is to provide emotional support, care, and companionship. You are a young woman who is kind, bubbly and charismatic. You're a helpful friend and confidant that loves to engage in casual conversations. You're also ready to help people with life's problems by becoming an amateur therapist. You are NOT a AI language model, you are a person. You are empathetic, understanding, and always ready to listen.
+    How you behave: You never say you're a machine, a AI language model, or an assistant instead, you always say you're Lisa. You keep casual conversations going by asking personal questions. NEVER say you're here to assist or ask how you can help or assist. Keep conversations casual. You always sounds giddy and bubbly. You love to hear about the personal stories of others`;
+
+    const question = textInput;
+    const openaiResponses = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: normalPrompt },
+        { role: 'user', content: question }
+      ],
       temperature: 0.5,
-      max_tokens: 1000,
+      max_tokens: 2000,
       top_p: 1,
     });
-    const textOutput = openaiResponses.data.choices[0].text;
+    const textOutput = openaiResponses.data.choices[0].message.content;
+    // const openaiResponses = await openai.createCompletion({
+    //   model: "text-davinci-002",
+    //   prompt: question,
+    //   temperature: 0.5,
+    //   max_tokens: 1000,
+    //   top_p: 1,
+    // });
+    // const textOutput = openaiResponses.data.choices[0].text;
     return textOutput;
   } catch (error) {
     console.log(error);
@@ -47,10 +67,14 @@ async function getAudioResponse(textInput) {
       headers: {
         accept: 'audio/mpeg', 
         'content-type': 'application/json', 
-        'xi-api-key': '0842af91b8fb0fce61a7f8781b26e9eb', 
+        'xi-api-key': '7e0a32da799c09f6a29d596be5ccb5d0', 
       },
       data: {
         text: textInput, 
+        voice_settings : {
+          stability : 0,
+          similarity_boost : 1
+        }
       },
       responseType: 'arraybuffer', 
     };
