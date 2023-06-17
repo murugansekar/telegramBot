be0f41325f2c3d83ea99c356ca4fa4628e01ddc9
MURUGAN SEKAR <murugansekar2608@gmail.com>
2023-06-17 18:38:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World  dummy ');
-};
+});
 app.listen(3000, () => {
   console.log('Server running");
 });
