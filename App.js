6ed20f9b26f8a3fc0811a1105f604db0ef7579d7
MURUGAN SEKAR <murugansekar2608@gmail.com>
2023-06-17 18:33:29 +0530

    Update App.js ljsds

--- App.js
+++ App.js
@@ -7,6 +7,18 @@ const axios = require('axios');
 const fs = require('fs');
 let token = process.env.TelegramKey;
 token = token.toString();
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World  dummy ');
+}
+app.listen(3000, () => {
+  console.log('Server running");
+});
+
+
+
 let Bot = new telegramBot(token, { polling: true });
 
 Bot.on("message", async (message) => {
