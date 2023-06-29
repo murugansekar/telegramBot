bd43a6a8e7acff3877f500f01a71c01295471e2c
MURUGAN SEKAR <murugansekar2608@gmail.com>
2023-06-29 21:47:09 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const Sequelize = require('sequelize')
+const db = new Sequelize(process.env.DB_LINK, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: 'true'
+          }
+    }
+  });
+module.exports = db;
